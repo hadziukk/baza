621a1b8e8693677422e0742d4a8df4fa713198da
hadziukk <127852832+hadziukk@users.noreply.github.com>
2024-10-02 07:35:43 +0000

    3

--- form.js
+++ form.js
@@ -1,4 +1,5 @@
 const mysql = require('mysql2');
+
 //połączenie
 const connection = mysql.createConnection({
   host: 'localhost',      
@@ -7,26 +8,42 @@ const connection = mysql.createConnection({
   database: 'Baza' 
 });
 console.log("test");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.send(`
+        <form action="/submit" method="POST">
+            <label>Wpisz Imię:</label>
+            <input type="text" id="imie" name="imie" required>
+            <br>
+            <label>Wpisz Nazwisko:</label>
+            <input type="text" id="nazwisko" name="nazwisko" required>
+            <br>
+            <button type="submit">Wyślij</button>
+        </form>
+    `);
+});
+
 // Połącz z bazą danych
 connection.connect((err) => {
   if (err) {
     return console.error('Błąd połączenia z bazą danych: ' + err.stack);
   }
- /* 
-  connection.query('Insert into dane Values(null,"imie","nazwisko")', (err, results) => {
-    if (err) {
-      return console.error('Błąd podczas zapytania: ' + err.stack);
-    }
-*/
-// Wykonaj SELECT
+  // Wykonaj SELECT
+  
   connection.query('SELECT * FROM dane', (err, results) => {
     if (err) {
       return console.error('Błąd podczas zapytania: ' + err.stack);
     }
+
     // Wyświetl dane
     console.log('Wyniki zapytania:');
     console.log(results);
+
     // Zamknij połączenie
     connection.end();
+    
+
   });
 });
